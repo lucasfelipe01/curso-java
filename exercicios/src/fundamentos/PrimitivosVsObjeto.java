a9073e0fb4c3cd6037eafc8503ae10c90947e251
lucasfelipe01 <lucas_felipe222@hotmail.com]>
2025-01-28 21:06:06 -0300

    Adiconando os execicios de PrimitivosVsObjeto e Wrappers

---
+++ exercicios/src/fundamentos/PrimitivosVsObjeto.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class PrimitivosVsObjeto {
+	
+	public static void main(String[] args) {
+		String s = new String("texto");
+		s.toUpperCase();
+		
+		
+		//Wrappers são a versã objetos dos tipos primitivos
+		int a =123;
+		System.out.println(a);
+		
+	}
+
+}
