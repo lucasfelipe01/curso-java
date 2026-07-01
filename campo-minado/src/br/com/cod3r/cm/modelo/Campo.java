e1c0df8cdaec45db9a2fee754fd264565106c927
lucasfelipe01 <lucas_felipe222@hotmail.com>
2026-06-30 22:16:24 -0300

    Adicionando o comando abrir

--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -3,6 +3,8 @@ package br.com.cod3r.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 public class Campo {
 	
 	private final int linha;
@@ -40,5 +42,35 @@ public class Campo {
 			return false;
 		}
 
+	}
+	
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	boolean abrir() {
+		
+		if (!aberto && !marcado) {
+			aberto = true;
+			
+			if(minado) {
+				throw new ExplosaoException();
+			}
+			
+			if (vizinhacaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	boolean vizinhacaSegura() {
+		return vizinhos.stream().noneMatch(v-> v.minado);
 	}
 }
